fa9774b8fbf482aad2e19c216a0f21cd39bab3ad
Mariana Rios Silveira Carvalho <marianariossc@gmail.com>
2024-08-13 01:03:05 -0400

    feat: Add initial project.

---
+++ iPokedex/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  iPokedex
+//
+//  Created by Mariana Rios Silveira Carvalho on 2024-08-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("iPokédex")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iPokedex/iPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPokedexApp.swift
+//  iPokedex
+//
+//  Created by Mariana Rios Silveira Carvalho on 2024-08-13.
+//
+
+import SwiftUI
+
+@main
+struct iPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
